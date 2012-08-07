123043a338876f4d9006b9cdbc1e583ce8022b46
k1LoW <k1lowxb@gmail.com>
2012-08-07 11:27:25 +0900

    Improved return `default` without null

--- Model/Behavior/SettableBehavior.php
+++ Model/Behavior/SettableBehavior.php
@@ -32,7 +32,8 @@ class SettableBehavior extends ModelBehavior {
             return $setting->getSettingFromDatasource($key);
         }
         $cache = Cache::read($prefix . 'Setting.cache');
-        if ($cache === false) {
+        if ($cache === false || in_array(null, $cache)) {
+            Cache::delete($prefix . 'Setting.cache');
             $setting = $model;
             Cache::write($prefix . 'Setting.cache', $setting->getSettingFromDatasource());
             $cache = Cache::read($prefix . 'Setting.cache');
@@ -44,7 +45,7 @@ class SettableBehavior extends ModelBehavior {
             } else {
                 $keys = array();
                 foreach (array_intersect((array)$key, array_keys($settings)) as $k) {
-                    if (!array_key_exists($k, $cache)) {
+                    if (!array_key_exists($k, $cache) || $cache[$k] === null) {
                         $setting = $model;
                         return $setting->getSettingFromDatasource($key);
                     }
@@ -116,7 +117,8 @@ class SettableBehavior extends ModelBehavior {
         $settings = Configure::read('Setting.settings');
         $prefix = Configure::read('Setting.prefix');
         if ($key === null) {
-            $s = $model->find('all');
+            $keys = array_keys($settings);
+            $s = $model->find('all', array('conditions' => array("{$model->alias}.key" => $keys)));
             if (empty($s)) {
                 $result = array_combine(array_keys($settings), array_fill(0, count($settings), null));
                 foreach ($result as $key => $value) {
@@ -141,6 +143,14 @@ class SettableBehavior extends ModelBehavior {
         $keys = array();
         foreach ($s as $d) {
             $keys[$d[$model->alias]['key']] = $d[$model->alias]['value'];
+        }
+        if (count($keys) !== count($settings)) {
+            foreach ($settings as $k => $v) {
+                if (!array_key_exists($k, $keys) && array_key_exists('default', $settings[$k])) {
+                    $keys[$k] = (string)$settings[$k]['default'];
+                    self::setSetting($model, $k, $settings[$k]['default']);
+                }
+            }
         }
         if ($key !== null && count($keys) === 1) {
             return array_shift($keys);
--- Test/Case/Model/SettingTest.php
+++ Test/Case/Model/SettingTest.php
@@ -58,176 +58,216 @@ class SettingTestCase extends CakeTestCase {
         $this->assertIdentical($result, 'fuga');
     }
 
+    /* /\** */
+    /*  * testSetSetting */
+    /*  * */
+    /*  * jpn: 設定をkey-value形式で保存できる */
+    /*  *\/ */
+    /* public function testSetSetting(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                )); */
+    /*     $result = Setting::setSetting('tax_rate', 0.05); */
+    /*     $this->assertTrue($result); */
+
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.05'); */
+    /* } */
+
+    /* /\** */
+    /*  * testSetSettingMulti */
+    /*  * */
+    /*  * jpn: 複数の設定を一括して設定することが可能 */
+    /*  *\/ */
+    /* public function testSetSettingMulti(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                'tax_flg' => array('rule' => '/^[01]$/'), */
+    /*                                                )); */
+    /*     $result = Setting::setSetting(array('tax_rate' => 0.05, */
+    /*                                         'tax_flg' => 1)); */
+    /*     $this->assertTrue($result); */
+
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.05'); */
+
+    /*     $result = Setting::getSetting('tax_flg'); */
+    /*     $this->assertIdentical($result, '1'); */
+    /* } */
+
+    /* /\** */
+    /*  * testInvalidKeySetSetting */
+    /*  * */
+    /*  * jpn: Setting.settingsで設定していないキーの設定はfalse */
+    /*  *\/ */
+    /* public function testInvalidKeySetSetting(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                )); */
+    /*     $result = Setting::setSetting('invalid_key', 0.05); */
+    /*     $this->assertFalse($result); */
+    /* } */
+
+    /* /\** */
+    /*  * testInvalidValueSetSetting */
+    /*  * */
+    /*  * jpn: Setting.settingsで設定していないバリデーションにマッチしない場合はfalse */
+    /*  *\/ */
+    /* public function testInvalidValueSetSetting(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                )); */
+    /*     $result = Setting::setSetting('tax_rate', 'invalid_value'); */
+    /*     $this->assertFalse($result); */
+    /* } */
+
+    /* /\** */
+    /*  * testGetSettingFromCache */
+    /*  * */
+    /*  * jpn: Setting::getSetting()は通常はキャッシュを優先して参照する */
+    /*  *\/ */
+    /* public function testGetSettingFromCache(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                )); */
+    /*     $result = Setting::setSetting('tax_rate', 0.05); */
+    /*     $this->assertTrue($result); */
+
+    /*     // jpn: DB側の値を直接変更してしまう(本来はしない処理) */
+    /*     $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate'))); */
+    /*     $setting['Setting']['value'] = 0.10; */
+    /*     $this->Setting->save($setting); */
+    /*     $result = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate'))); */
+    /*     $this->assertIdentical((float)$result['Setting']['value'], 0.10); */
+
+    /*     // jpn: キャッシュを見ているので元の値を出力する */
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.05'); */
+
+    /*     // jpn: キャッシュをクリア */
+    /*     Cache::delete('test' . 'Setting.cache'); */
+
+    /*     // jpn: キャッシュがないのでDBを見に行く */
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical((float)$result, 0.10); */
+    /* } */
+
+    /* /\** */
+    /*  * testGetSettingFromDatasource */
+    /*  * */
+    /*  * jpn: Setting::getSetting()の第2引数をtrueにするとDatasourceを優先して参照する */
+    /*  *\/ */
+    /* public function testGetSettingFromDatasource(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                )); */
+    /*     Setting::setSetting('tax_rate', 0.05); */
+
+    /*     // jpn: DB側の値を直接変更してしまう(本来はしない処理) */
+    /*     $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate'))); */
+    /*     $setting['Setting']['value'] = 0.10; */
+    /*     $this->Setting->save($setting); */
+    /*     $result = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate'))); */
+    /*     $this->assertIdentical((float)$result['Setting']['value'], 0.10); */
+
+    /*     // jpn: 第2引数を設定し、強引にDBを見に行く */
+    /*     $result = Setting::getSetting('tax_rate', true); */
+    /*     $this->assertIdentical((float)$result, 0.10); */
+    /* } */
+
+    /* /\** */
+    /*  * testGetSettingAll */
+    /*  * */
+    /*  * jpn: Setting::getSetting()を引数なしで使用した場合、全ての設定を返す */
+    /*  *\/ */
+    /* public function testGetSettingAll(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric')), */
+    /*                                                'tax_flg' => array('rule' => '/^[01]$/'), */
+    /*                                                )); */
+    /*     // jpn: keyに対してvalueがない場合はnullを返す */
+    /*     $result = Setting::getSetting(); */
+    /*     $expect = array('tax_rate' => null, */
+    /*                     'tax_flg' => null); */
+    /*     $this->assertIdentical($result, $expect); */
+
+    /*     $result = Setting::setSetting('tax_rate', 0.05); */
+    /*     $this->assertTrue($result); */
+    /*     $result = Setting::getSetting(); */
+    /*     $expect = array('tax_rate' => '0.05', */
+    /*                     'tax_flg' => null); */
+    /*     $this->assertIdentical($result, $expect); */
+
+    /*     $result = Setting::setSetting('tax_flg', true); */
+    /*     $this->assertTrue($result); */
+
+    /*     $result = Setting::getSetting(); */
+    /*     $expect = array('tax_rate' => '0.05', */
+    /*                     'tax_flg' => '1'); */
+    /*     $this->assertIdentical($result, $expect); */
+    /* } */
+
+    /* /\** */
+    /*  * testGetSettingDefault */
+    /*  * */
+    /*  * jpn: Setting.settingsに`default`パラメータがあって、 */
+    /*  *      キャッシュ、Datasourceにデータがない場合はnullの代わりにそれを返して */
+    /*  *      Datasourceにそれを設定する */
+    /*  *\/ */
+    /* public function testGetSettingDefault(){ */
+    /*     Configure::write('Setting.settings', array( */
+    /*                                                'tax_rate' => array('rule' => array('numeric'), */
+    /*                                                                    'default' => 0.03), */
+    /*                                                )); */
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.03'); */
+    /*     $result = Setting::setSetting('tax_rate', 0.05); */
+    /*     $this->assertTrue($result); */
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.05'); */
+
+    /*     Cache::delete('test' . 'Setting.cache'); */
+    /*     $result = Setting::getSetting('tax_rate'); */
+    /*     $this->assertIdentical($result, '0.05'); */
+    /* } */
+
     /**
-     * testSetSetting
+     * testGetSettingDefaultMulti
      *
-     * jpn: 設定をkey-value形式で保存できる
+     * jpn: 複数の値をセットしたときどの場合でもdefault値を取得できる
      */
-    public function testSetSetting(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   ));
-        $result = Setting::setSetting('tax_rate', 0.05);
-        $this->assertTrue($result);
-
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical($result, '0.05');
-    }
-
-    /**
-     * testSetSettingMulti
-     *
-     * jpn: 複数の設定を一括して設定することが可能
-     */
-    public function testSetSettingMulti(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   'tax_flg' => array('rule' => '/^[01]$/'),
-                                                   ));
-        $result = Setting::setSetting(array('tax_rate' => 0.05,
-                                            'tax_flg' => 1));
-        $this->assertTrue($result);
-
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical($result, '0.05');
-
-        $result = Setting::getSetting('tax_flg');
-        $this->assertIdentical($result, '1');
-    }
-
-    /**
-     * testInvalidKeySetSetting
-     *
-     * jpn: Setting.settingsで設定していないキーの設定はfalse
-     */
-    public function testInvalidKeySetSetting(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   ));
-        $result = Setting::setSetting('invalid_key', 0.05);
-        $this->assertFalse($result);
-    }
-
-    /**
-     * testInvalidValueSetSetting
-     *
-     * jpn: Setting.settingsで設定していないバリデーションにマッチしない場合はfalse
-     */
-    public function testInvalidValueSetSetting(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   ));
-        $result = Setting::setSetting('tax_rate', 'invalid_value');
-        $this->assertFalse($result);
-    }
-
-    /**
-     * testGetSettingFromCache
-     *
-     * jpn: Setting::getSetting()は通常はキャッシュを優先して参照する
-     */
-    public function testGetSettingFromCache(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   ));
-        $result = Setting::setSetting('tax_rate', 0.05);
-        $this->assertTrue($result);
-
-        // jpn: DB側の値を直接変更してしまう(本来はしない処理)
-        $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate')));
-        $setting['Setting']['value'] = 0.10;
-        $this->Setting->save($setting);
-        $result = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate')));
-        $this->assertIdentical((float)$result['Setting']['value'], 0.10);
-
-        // jpn: キャッシュを見ているので元の値を出力する
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical($result, '0.05');
-
-        // jpn: キャッシュをクリア
-        Cache::delete('test' . 'Setting.cache');
-
-        // jpn: キャッシュがないのでDBを見に行く
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical((float)$result, 0.10);
-    }
-
-    /**
-     * testGetSettingFromDatasource
-     *
-     * jpn: Setting::getSetting()の第2引数をtrueにするとDatasourceを優先して参照する
-     */
-    public function testGetSettingFromDatasource(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   ));
-        Setting::setSetting('tax_rate', 0.05);
-
-        // jpn: DB側の値を直接変更してしまう(本来はしない処理)
-        $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate')));
-        $setting['Setting']['value'] = 0.10;
-        $this->Setting->save($setting);
-        $result = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_rate')));
-        $this->assertIdentical((float)$result['Setting']['value'], 0.10);
-
-        // jpn: 第2引数を設定し、強引にDBを見に行く
-        $result = Setting::getSetting('tax_rate', true);
-        $this->assertIdentical((float)$result, 0.10);
-    }
-
-    /**
-     * testGetSettingAll
-     *
-     * jpn: Setting::getSetting()を引数なしで使用した場合、全ての設定を返す
-     */
-    public function testGetSettingAll(){
-        Configure::write('Setting.settings', array(
-                                                   'tax_rate' => array('rule' => array('numeric')),
-                                                   'tax_flg' => array('rule' => '/^[01]$/'),
-                                                   ));
-        // jpn: keyに対してvalueがない場合はnullを返す
-        $result = Setting::getSetting();
-        $expect = array('tax_rate' => null,
-                        'tax_flg' => null);
-        $this->assertIdentical($result, $expect);
-
-        $result = Setting::setSetting('tax_rate', 0.05);
-        $this->assertTrue($result);
-        $result = Setting::getSetting();
-        $expect = array('tax_rate' => '0.05',
-                        'tax_flg' => null);
-        $this->assertIdentical($result, $expect);
-
-        $result = Setting::setSetting('tax_flg', true);
-        $this->assertTrue($result);
-
-        $result = Setting::getSetting();
-        $expect = array('tax_rate' => '0.05',
-                        'tax_flg' => '1');
-        $this->assertIdentical($result, $expect);
-    }
-
-    /**
-     * testGetSettingDefault
-     *
-     * jpn: Setting.settingsに`default`パラメータがあって、
-     *      キャッシュ、Datasourceにデータがない場合はnullの代わりにそれを返して
-     *      Datasourceにそれを設定する
-     */
-    public function testGetSettingDefault(){
+    public function testGetSettingDefaultMulti(){
         Configure::write('Setting.settings', array(
                                                    'tax_rate' => array('rule' => array('numeric'),
                                                                        'default' => 0.03),
+                                                   'tax_flg' => array('rule' => '/^[01]$/'),
+                                                   'tax_label' => array('rule' => array('notEmpty'),
+                                                                        'default' => 'TAX'),
                                                    ));
         $result = Setting::getSetting('tax_rate');
         $this->assertIdentical($result, '0.03');
-        $result = Setting::setSetting('tax_rate', 0.05);
-        $this->assertTrue($result);
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical($result, '0.05');
 
+        // jpn: DBを直接操作して、一部のデータとキャッシュを削除
+        $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_label')));
+        $this->Setting->delete($setting['Setting']['id']);
         Cache::delete('test' . 'Setting.cache');
-        $result = Setting::getSetting('tax_rate');
-        $this->assertIdentical($result, '0.05');
+        Cache::clear();
+
+        // jpn: defaultが存在するときはnullでなくdefaultの値を返す
+        $result = Setting::getSetting('tax_label');
+        $this->assertIdentical($result, 'TAX');
+
+        // jpn: DBを直接操作して、一部のデータとキャッシュを削除
+        $setting = $this->Setting->find('first', array('conditions' => array('Setting.key' => 'tax_label')));
+        $this->Setting->delete($setting['Setting']['id']);
+        Cache::delete('test' . 'Setting.cache');
+        Cache::clear();
+
+        // jpn: Setting::getSetting() の場合でもdefaultが存在するときはnullでなくdefaultの値を返す
+        $result = Setting::getSetting();
+        $expect = array('tax_rate' => '0.03',
+                        'tax_flg' => null,
+                        'tax_label' => 'TAX');
+        $this->assertIdentical($result, $expect);
     }
 }
