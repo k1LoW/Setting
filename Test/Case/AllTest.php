116913d9bdaad5fe6247eee3805529c0b494102e
k1LoW <k1lowxb@gmail.com>
2013-05-10 09:10:03 +0900

    Use coveralls.io

---
+++ Test/Case/AllTest.php
@@ -0,0 +1,9 @@
+<?php
+class AllTest extends CakeTestSuite {
+    public static function suite() {
+        $suite = new CakeTestSuite('All tests');
+        $dir = dirname(__FILE__);
+        $suite->addTestDirectory($dir . DS . 'Model');
+        return $suite;
+    }
+}
